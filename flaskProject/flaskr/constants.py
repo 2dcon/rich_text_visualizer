91ade11d8720997dde88378e2dc175ba64f7b9e3
2dcon <2jicon@gmail.com>
2022-03-17 20:08:22 +0800

    Update constants.py

--- flaskProject/flaskr/constants.py
+++ flaskProject/flaskr/constants.py
@@ -1,39 +1,109 @@
-FILE = r'bilingual'
+import os
+import string
+from flaskr import constants
+from flask import Flask, request, render_template
+from datetime import datetime
 
-# title for the columns of the source/target language
-SOURCE_COL = 'zh'
-TARGET_COL = 'ja'
+here = os.path.dirname(__file__)
 
-FONT_COLOR = 'd9d5d6'
-BACKGROUND_COLOR = '322b28'
-# characters that marks the in-text scripts
-DELIMITER = ('[', ']', '-')
-PLACEHOLDER = ('{', '}')
-SP_CHAR = ['$', '_', '|']
-BACKSLASH = '\\'
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-ROW_OFFSET = 2
 
-# escape sequences
-ESCAPE_CHAR = {'n': '\n', 'r': '\r', 't': '\t', 'b': '\b', 'f': '\f'}
+def checkCode(text0, text1, result):
+    list0 = []
+    list1 = []
 
-# html stuffs
-TAG_P = '<p class="textbox" style="color:#' + FONT_COLOR + ';">'
-TAG_CLOSE = {'h': '</h>', 'p': '</p>', 'html': '</html>'}
-NEWLINE = '<br>'
-LINK = '<!DOCTYPE html><html><head>' \
-       '<link rel= "stylesheet" type= "text/css" href= "{{ url_for(\'static\',filename=\'styles/main.css\') }}">' \
-       '</head>'
-div = ['<div class="parent">', '<div class="child0">', '<div class="child1">', '</div>']
+    return
 
-# test strings
-test0 = str('\n')
-test1 = r'$$[57ea57]意地っ張りな少年{0}[-]と農場のことを尋ねる'
-test2 = '[de2524][デビアス]を開放{開放した}した後に\n受注可能[-]$$$町にいる[57ea57]{0}[-]を探そう{abcdefg}'
-test3 = '[57ea57][57ea57]part0[-] part1 [57ea57]part2[de2524]part3[-][-]'
-test4 = '[57ea57]part0[-]'
-test5 = '[57ea57][de2524]part0[-][-]'
-test6 = '[57ea57]part0[-][de2524]part1[-]'
-test7 = '[57ea57]part0[de2524]part1[-]part2[abc123]part3[-]part4[-]npart5'
-test8 = '[57ea57]part0[de2524]part1[-][-][de2524]part2[-]'
-test9 = '[aaa111]p0[bbb222]p1[ccc333]p2[ddd444]p3[-]p4[-]p5[-][-]'
+
+def convertEscape(text: string):
+    new_text = text
+    idx = 0
+    while idx < len(new_text):
+        nxt = idx + 1
+        if new_text[idx] == '\\' and new_text[nxt] == 'n':
+            new_text = new_text[:idx] + constants.NEWLINE + new_text[idx + 2:]
+        idx += 1
+    return new_text
+
+
+def isHex(string6: string):
+    if len(string6) == 6:
+        for char in string6:
+            if not (char in string.hexdigits):
+                return False
+
+    return True
+
+
+def getHTML(hex_color: string):
+    return '<h style="color:#' + hex_color + ';">'
+
+
+def getRich(text: string):
+    idx = 0
+    rtxt: string = constants.TAG_P
+    eot = len(text)
+
+    while idx < eot:
+        step = 1
+        is_code = False
+        if text[idx] == constants.DELIMITER[0]:
+            opening = idx + 7
+            closing = idx + 2
+
+            if opening < eot and text[opening] == constants.DELIMITER[1]:
+                hcode = text[idx + 1:opening]
+                if isHex(hcode):
+                    rtxt += getHTML(hcode)
+                    step += 7
+                    is_code = True
+            if closing < eot and text[idx + 1] == constants.DELIMITER[2] and text[closing] == constants.DELIMITER[1]:
+                rtxt += constants.TAG_CLOSE['h']
+                step += 2
+                is_code = True
+
+        # regular text
+        if not is_code:
+            rtxt += text[idx]
+        idx += step
+        # reach eot
+
+    rtxt += constants.TAG_CLOSE['p']
+    return convertEscape(rtxt)
+
+
+def genPage(code: string):
+    page = open(here + '/templates/result.html', 'w')
+    page.write(code)
+    page.close()
+
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
+
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+@app.route('/', methods=['POST'])
+def submit():
+    raw0 = request.form['textbox_0']
+    raw1 = request.form['textbox_1']
+
+    rt0 = getRich(raw0)
+    rt1 = getRich(raw1)
+
+    page = constants.LINK + constants.div[0] + constants.div[1] + rt0 + constants.div[3] + constants.div[2] + rt1 \
+           + constants.div[3] + constants.TAG_CLOSE['html']
+    genPage(page)
+
+    with open(here + '/stats/log', 'a') as rec:
+        rec.write(datetime.now().strftime('%Y/%m/%d - %H:%M:%S: ') +
+                  request.environ.get('HTTP_X_REAL_IP', request.remote_addr) + '\n' + raw0 + '\n' + raw1 + '\n\n' +
+                  '=============================================================\n\n')
+
+    return render_template('result.html')
